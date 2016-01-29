a9bca106f7afeaa0e0dbf47f35af40fb70ec0ef2
Mateusz <mateusz@dcx.bz>
2016-01-29 21:29:22 +0100

    navigator test

---
+++ PolyJam2016/Assets/Scripts/WalkerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkerController : MonoBehaviour {
+
+	NavMeshAgent navMeshAgent;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		navMeshAgent.SetDestination (target.position);
+	}
+}
