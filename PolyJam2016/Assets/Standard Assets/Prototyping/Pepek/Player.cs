33debc89ac3a1f0050f1582a6f85439434472daa
Pepek91 <michalstep@wp.pl>
2016-01-29 21:33:05 +0100

    Players test

---
+++ PolyJam2016/Assets/Standard Assets/Prototyping/Pepek/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 15;
+    public bool isPlayerOne = true;
+
+    private string horizontal;
+    private string vertical;
+    private string action;
+
+    // Use this for initialization
+    void Start () {
+        if(isPlayerOne)
+        {
+            horizontal = "Player One Horizontal";
+            vertical = "Player One Vertical";
+            action = "Player One Action";
+        }
+        else
+        {
+            horizontal = "Player Two Horizontal";
+            vertical = "Player Two Vertical";
+            action = "Player Two Action";
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition += new Vector3(Input.GetAxis(horizontal) * speed * Time.deltaTime, 0, Input.GetAxis(vertical) * speed * Time.deltaTime);
+
+        transform.position = newPosition;
+    }
+
+}
