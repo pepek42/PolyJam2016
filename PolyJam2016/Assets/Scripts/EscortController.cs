bc19b3f3ea2c2bc074958692ba42ac915e0b0367
Mateusz <mateusz@dcx.bz>
2016-01-31 12:17:25 +0100

    escort models

---
+++ PolyJam2016/Assets/Scripts/EscortController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscortController : MonoBehaviour {
+
+	Animator[] animators;
+
+	// Use this for initialization
+	void Start () {
+		animators = GetComponentsInChildren<Animator> ();
+		foreach (Animator animator in animators) {
+			animator.SetBool ("walking", true);
+		}
+	}
+}
